0789ee4231a3a580e467e25e05e92ae4028263b5
Israt-Jahan-Sujana <isratsujama25@gmail.com>
2025-05-09 07:05:48 +0600

    first part done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module .exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
 }
+
+
